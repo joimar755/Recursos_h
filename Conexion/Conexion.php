da2b493d75ae1de302c9a81aa9e9aa6c6ac4b6a2
joimar755 <joimarjose19@gmail.com>
2021-12-05 11:11:13 -0500

    5/12/2021

--- Conexion/Conexion.php
+++ Conexion/Conexion.php
@@ -1,29 +1,29 @@
 <?php
 
-class Database{
+class Conexion{
 
-  private $hostname = "localhost"; 
-  private $database = "proyecto_y&s"; 
-  private $username = "root"; 
-  private $password = ""; 
-  private $charset = "utf8"; 
-
-    function conectar()
-    {
-        try {
-
-            $conexion = "mysql:host=" . $this->hostname . ";dbname=" . $this->database . ";charset=" . $this->charset;
-            $options = [
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_EMULATE_PREPARES => false,
-            ];
-
-            $pdo = new PDO($conexion, $this->username, $this->password, $options);
-
-            return $pdo;
-        } catch (PDOException $e) {
-            echo 'Error conexion: ' . $e->getMessage();
-            exit;
-        }
-    }
-}
+    private $hostname = "localhost"; 
+    private $database = "proyecto_y&s"; 
+    private $username = "root"; 
+    private $password = ""; 
+    private $charset = "utf8"; 
+  
+      function conectar()
+      {
+          try {
+  
+              $conexion = "mysql:host=" . $this->hostname . ";dbname=" . $this->database . ";charset=" . $this->charset;
+              $options = [
+                  PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                  PDO::ATTR_EMULATE_PREPARES => false,
+              ];
+  
+              $pdo = new PDO($conexion, $this->username, $this->password, $options);
+  
+              return $pdo;
+          } catch (PDOException $e) {
+              echo 'Error conexion: ' . $e->getMessage();
+              exit;
+          }
+      }
+  }
