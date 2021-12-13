1b21bb34fb596d862091463f216c173cc7fe913b
joimar755 <joimarjose19@gmail.com>
2021-12-13 16:57:35 -0500

    D

---
+++ ajax/validar_login.js
@@ -0,0 +1,18 @@
+/*  $(document).ready(function () {
+     $('#form').submit(function (e) { 
+         
+         const login  = {
+            user:$('#user').val(),
+            contrasena:$('#contrasena').val(),
+
+         }
+         console.log(login); 
+         
+         $('#form').trigger('reset');
+         
+         e.preventDefault();
+     });  
+     
+ 
+});  
+*/
